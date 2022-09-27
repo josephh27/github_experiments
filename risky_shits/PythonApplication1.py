5de38c0c2d4982b3829d75a8d12550e8ad9f23b3
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-09-27 14:02:00 +0800

    Back

--- risky_shits/PythonApplication1.py
+++ risky_shits/PythonApplication1.py
@@ -3,9 +3,12 @@ print("Joseph Jason Supelario Buhain")
 phrase = "fdsfdsfdsfdsfsdfsdfsd"
 print(phrase)
 print(phrase.replace("fd","ae"))
+<<<<<<< HEAD
 
 
 
 print("HHAHAHAHA")
 print("truthy and falsy and shift = pop and unshift = push")
 print("what the fuck")
+=======
+>>>>>>> parent of 9126b9b (Okay)
