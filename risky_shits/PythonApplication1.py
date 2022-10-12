8b0cb029dcb9f5945d48e6fc3dbf4d6ee4be4a26
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-10-12 18:25:31 +0800

    oraryt

--- risky_shits/PythonApplication1.py
+++ risky_shits/PythonApplication1.py
@@ -7,5 +7,15 @@ print(phrase.replace("fd","ae"))
 
 
 print("HHAHAHAHA")
+<<<<<<< HEAD
 print("truthy and falsy and shift = pop and unshift = push")
+<<<<<<< HEAD
 print("what the fuck")
+=======
+>>>>>>> parent of 9126b9b (Okay)
+=======
+#okay
+>>>>>>> final_shits
+=======
+print("what the fuck")
+>>>>>>> parent of 5de38c0 (Back)
