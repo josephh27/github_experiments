3b50b4ad859bd797a23a9d7ccd42d7eb80324fc0
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-11-08 22:50:54 +0800

    restoring th file

---
+++ risky_shitsss/Python_Application1.py
@@ -0,0 +1,41 @@
+
+print("Joseph Jason Supelario Buhain")
+phrase = "fdsfdsfdsfdsfsdfsdfsd"
+print(phrase)
+print(phrase.replace("fd","ae"))
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> e9595e4aa6813c0350fd29208db4b99d660ad53a
+
+#what the fuck
+print("HHAHAHAHA")
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("truthy and falsy and shift = pop and unshift = push")
+<<<<<<< HEAD
+print("what the fuck")
+=======
+>>>>>>> parent of 9126b9b (Okay)
+=======
+#okay
+>>>>>>> final_shits
+=======
+print("what the fuck")
+>>>>>>> parent of 5de38c0 (Back)
+=======
+>>>>>>> parent of 93ed3eb (Another okay)
+=======
+print("truthy and falsy and shift = pop and unshift = push")
+<<<<<<< HEAD
+>>>>>>> parent of cca281e (What the f)
+=======
+<<<<<<<< HEAD:risky_shitsss/PythonApplication1.py
+print("what the fuck")
+=======
+>>>>>>> parent of 9126b9b (Okay)
+========
+>>>>>>> parent of cca281e (What the f)
+>>>>>>>> e9595e4aa6813c0350fd29208db4b99d660ad53a:risky_shits/PythonApplication1.py
+>>>>>>> e9595e4aa6813c0350fd29208db4b99d660ad53a
