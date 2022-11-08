ed73a04fcf6171ad80424a9df5ddf4ad900987f2
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-11-08 22:13:18 +0800

    rename

---
+++ risky_shitsss/PythonApplication1.py
@@ -0,0 +1,27 @@
+
+print("Joseph Jason Supelario Buhain")
+phrase = "fdsfdsfdsfdsfsdfsdfsd"
+print(phrase)
+print(phrase.replace("fd","ae"))
+
+#what the
+print("HHAHAHAHA")
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("truthy and falsy and shift = pop and unshift = push")
+<<<<<<< HEAD
+print("what the fuck")
+=======
+>>>>>>> parent of 9126b9b (Okay)
+=======
+#okay
+>>>>>>> final_shits
+=======
+print("what the fuck")
+>>>>>>> parent of 5de38c0 (Back)
+=======
+>>>>>>> parent of 93ed3eb (Another okay)
+=======
+print("truthy and falsy and shift = pop and unshift = push")
+>>>>>>> parent of cca281e (What the f)
